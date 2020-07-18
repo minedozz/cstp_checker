f9a294987be843b3af661ca6af9b12ca99bddff5
DanilaKorobkov <k.danila67@gmail.com>
2020-07-18 21:35:12 +0300

    Правки вывода сообщения

--- checker/output_processor/log_errors_only_output_processor.py
+++ checker/output_processor/log_errors_only_output_processor.py
@@ -1,17 +1,12 @@
 from pathlib import Path
+from datetime import datetime
 
 from .i_output_processor import *
-
-url: str
-status: Status
-
-response_code: int = -1
-args_checks: Tuple[ArgCheckResult] = tuple()
-error_message: str = field(default='')
 
 
 class LogErrorsOnlyOutputProcessor(IOutputProcessor):
     def __init__(self, log_file: Path):
+        log_file = log_file.parent / f'{log_file.name}_{datetime.now()}'
         log_file.parent.mkdir(parents=True, exist_ok=True)
         log_file.touch(exist_ok=True)
 
@@ -19,19 +14,20 @@ class LogErrorsOnlyOutputProcessor(IOutputProcessor):
 
     async def process(self, output: OutputData):
         if not output.is_all_ok:
-            with self._log_file.open(encoding='utf-8') as stream:
-                stream.write(f'General result {output.is_all_ok}')
-                stream.write('Details:')
+            with self._log_file.open('a', encoding='utf-8') as stream:
+                stream.write(f'General result: {output.is_all_ok}\n')
+                stream.write('Details: ')
 
                 for detail in output.details:
                     if not detail.is_all_ok:
-                        message = [f'Check of {detail.url}',
+                        message = [f'\nCheck of {detail.url}',
+                                   f'Status: {detail.status.name}',
                                    f'General result: {detail.is_all_ok}',
                                    f'Response code: {detail.response_code}',
                                    f'Error message: {detail.error_message}']
-                        missing_args = '\n'.join(arg.arg_name for arg in detail.missing_args)
+                        missing_args = ', '.join(arg.arg_name for arg in detail.missing_args)
                         message.append(f'Missing args: {missing_args}')
 
-                        message = '\n'.join(message)
-                        stream.write(message)
-                        stream.write('\n')
+                        message = '\n\t'.join(message)
+                        stream.write(f'\t{message}')
+                        stream.write('\n\n')
